cfb1e99f781f8cc5478e9f1e687b912f96272a49
Jordan Murphy <jmurphy6895@gmail.com>
2024-09-24 16:54:32 -0600

    chore: differentiablity tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,9 @@ using LinearAlgebra
 using SatelliteToolboxTransformations
 using Scratch
 
+using DifferentiationInterface
+import FiniteDiff, ForwardDiff
+
 # We must clear the scratch space before the tests to avoid errors.
 clear_scratchspaces!(SatelliteToolboxGravityModels)
 
@@ -17,4 +20,8 @@ end
 
 @testset "GravityModels API" verbose = true begin
     include("./gravity_models.jl")
+end
+
+@testset "Differentiation Tests" verbose = true begin
+    include("differentiability.jl")
 end
