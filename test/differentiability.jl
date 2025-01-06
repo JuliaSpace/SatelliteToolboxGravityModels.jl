ccbb6438663cf0a5a913304d39a99b53cb129d5c
Jordan Murphy <jmurphy6895@gmail.com>
2025-01-06 16:32:49 -0700

    chore: temp commit

--- test/differentiability.jl
+++ test/differentiability.jl
@@ -12,35 +12,42 @@ const _GRAVITY_MODELS = (
 
 const _BACKENDS = (
     ("ForwardDiff", AutoForwardDiff()),
+    ("Diffractor", AutoDiffractor()),
+    ("Enzyme", AutoEnzyme()),
+    ("Mooncake", AutoMooncake(;config=nothing)),
+    ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
+    ("Zygote", AutoZygote()),
 )
 
 @testset "State Automatic Differentiation" begin
+    for backend in _BACKENDS
+        testset_name = "Gravity Models " * string(backend[1])
+        @testset "$testset_name" begin
+            for t in _GRAVITY_MODELS
+                model = GravityModels.load(IcgemFile, fetch_icgem_file(t[1]))
+                lat   = 27.5
+                lon   = 235.3
+                h     = 0.0
 
-    for t in _GRAVITY_MODELS
-        model = GravityModels.load(IcgemFile, fetch_icgem_file(t[1]))
-        lat   = 27.5
-        lon   = 235.3
-        h     = 0.0
+                # Use the model to compute the gravity using all the coefficients.
+                r_itrf = geodetic_to_ecef(lat, lon, 0)
 
-        # Use the model to compute the gravity using all the coefficients.
-        r_itrf = geodetic_to_ecef(lat, lon, 0)
-
-        f_fd, df_fd = value_and_jacobian(
-            (x) -> GravityModels.gravitational_acceleration(model, x),
-            AutoFiniteDiff(),
-            r_itrf
-        )
-
-        for backend in _BACKENDS
-            @eval @testset $(string(t[1]) * " " * string(backend[1])) begin
+                f_fd, df_fd = value_and_jacobian(
+                    (x) -> GravityModels.gravitational_acceleration(model, x),
+                    AutoFiniteDiff(),
+                    r_itrf
+                )
+            
                 f_ad, df_ad = value_and_jacobian(
-                    (x) -> GravityModels.gravitational_acceleration($model, x),
-                    $backend[2],
-                    $r_itrf
+                    (x) -> GravityModels.gravitational_acceleration(model, x)[1],
+                    backend[2],
+                    r_itrf
                 )
 
-                @test $f_fd == f_ad
-                @test $df_fd ≈ df_ad rtol=1e-4
+                println(values(df_ad))
+
+                @test f_fd == f_ad
+                @test df_fd ≈ df_ad rtol=1e-4
             end
         end
     end
