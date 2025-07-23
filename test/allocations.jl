c0152e4d0b78fc636c936447105ddddc91c18a3b
Jordan Murphy <jmurphy6895@gmail.com>
2025-07-23 10:59:39 +0300

    chore: cleanup compat and fix aqua

--- test/allocations.jl
+++ test/allocations.jl
@@ -4,7 +4,7 @@
 #
 ############################################################################################
 @testset "Aqua.jl" begin
-    Aqua.test_all(SatelliteToolboxGravityModels; ambiguities=(recursive = false))
+    Aqua.test_all(SatelliteToolboxGravityModels; ambiguities=(recursive = false), deps_compat=(check_extras = false))
 end
 
 @testset "JET Testing" begin
