440d9b432dc174aa21b8d4e66a6db889efa21454
Jordan Murphy <jmurphy6895@gmail.com>
2025-08-01 15:58:39 +0300

    chore: fix how Mooncake and Enzyme are loaded

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,8 @@ if isempty(VERSION.prerelease)
     # Add Mooncake and Enzyme to the project if not the nightly version
     # Adding them via the Project.toml isn't working because it tries to compile them before reaching the gating
     using Pkg
-    Pkg.add("Mooncake", "Enzyme")
+    Pkg.add("Mooncake")
+    Pkg.add("Enzyme")
 
     # Test with Mooncake and Enzyme along with the other backends
     using Mooncake, Enzyme
