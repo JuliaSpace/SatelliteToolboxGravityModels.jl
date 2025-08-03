43f908af26fc647e065daf084ddf9b05f9e9a50b
Jordan Murphy <jmurphy6895@users.noreply.github.com>
2025-08-04 01:22:38 +0300

    :wrench: Gate non-core tests for nightly CI (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,47 +8,6 @@ using LinearAlgebra
 using SatelliteToolboxTransformations
 using Scratch
 
-using DifferentiationInterface
-using FiniteDiff, ForwardDiff, PolyesterForwardDiff, Zygote
-
-if isempty(VERSION.prerelease)
-    # Add Mooncake and Enzyme to the project if not the nightly version
-    # Adding them via the Project.toml isn't working because it tries to compile them before reaching the gating
-    using Pkg
-    Pkg.add("Mooncake")
-    Pkg.add("Enzyme")
-    Pkg.add("JET")
-    Pkg.add("AllocCheck")
-
-    # Test with Mooncake and Enzyme along with the other backends
-    using Mooncake, Enzyme
-    const _BACKENDS = (
-        ("ForwardDiff", AutoForwardDiff()),
-        ("Enzyme", AutoEnzyme()),
-        ("Mooncake", AutoMooncake(;config=nothing)),
-        ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
-        ("Zygote", AutoZygote()),
-    )
-
-    using JET
-    using AllocCheck
-
-    @testset "Performance and Memory Allocations" verbose = true begin
-        include("./allocations.jl")
-    end
-else
-    @warn "Mooncake.jl and Enzyme.jl not guaranteed to work on julia-nightly, skipping tests"
-    const _BACKENDS = (
-        ("ForwardDiff", AutoForwardDiff()),
-        ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
-        ("Zygote", AutoZygote()),
-    )
-
-    @warn "JET and AllocCheck not guaranteed to work on julia-nightly, skipping tests"
-end
-
-using Aqua
-
 # We must clear the scratch space before the tests to avoid errors.
 clear_scratchspaces!(SatelliteToolboxGravityModels)
 
@@ -60,11 +19,45 @@ end
     include("./gravity_models.jl")
 end
 
-@testset "Differentiation Tests" verbose = true begin
-    include("differentiability.jl")
-end
 
-@testset "Aqua.jl" begin
-    Aqua.test_all(SatelliteToolboxGravityModels; ambiguities=(recursive = false), deps_compat=(check_extras = false))
-end
+if isempty(VERSION.prerelease)
+    # Add Mooncake and Enzyme to the project if not the nightly version
+    # Adding them via the Project.toml isn't working because it tries to compile them before reaching the gating
+    using Pkg
+    Pkg.add("DifferentiationInterface")
+    Pkg.add("Enzyme")
+    Pkg.add("FiniteDiff")
+    Pkg.add("ForwardDiff")
+    Pkg.add("Mooncake")
+    Pkg.add("PolyesterForwardDiff")
+    Pkg.add("Zygote")
 
+    Pkg.add("JET")
+    Pkg.add("AllocCheck")
+    Pkg.add("Aqua")
+
+    # Test with Mooncake and Enzyme along with the other backends
+    using DifferentiationInterface
+    using Enzyme, FiniteDiff, ForwardDiff, Mooncake, PolyesterForwardDiff, Zygote
+    const _BACKENDS = (
+        ("ForwardDiff", AutoForwardDiff()),
+        ("Enzyme", AutoEnzyme()),
+        ("Mooncake", AutoMooncake(;config=nothing)),
+        ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
+        ("Zygote", AutoZygote()),
+    )
+
+    using JET
+    using AllocCheck
+    using Aqua
+
+    @testset "Performance and Memory Allocations" verbose = true begin
+        include("./allocations.jl")
+    end
+    @testset "Differentiation Tests" verbose = true begin
+        include("differentiability.jl")
+    end
+else
+    @warn "Differentiation backends not guaranteed to work on julia-nightly, skipping tests"
+    @warn "Performance checks not guaranteed to work on julia-nightly, skipping tests"
+end
