75bf346b24bd78da569254cb11959314edec2f79
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-10-04 22:44:57 -0300

    :rotating_light: Fix tests

--- test/allocations.jl
+++ test/allocations.jl
@@ -3,12 +3,22 @@
 # Tests related to performance and memory allocations.
 #
 ############################################################################################
+
 @testset "Aqua.jl" begin
-    Aqua.test_all(SatelliteToolboxGravityModels; ambiguities=(recursive = false), deps_compat=(check_extras = false))
+    Aqua.test_all(
+        SatelliteToolboxGravityModels;
+        ambiguities      = (recursive    = false),
+        deps_compat      = (check_extras = false),
+        persistent_tasks = false
+    )
 end
 
 @testset "JET Testing" begin
-    rep = JET.test_package(SatelliteToolboxGravityModels; toplevel_logger=nothing, target_modules=(@__MODULE__,))
+    rep = JET.test_package(
+        SatelliteToolboxGravityModels;
+        toplevel_logger = nothing,
+        target_modules  = (@__MODULE__,)
+    )
 end
 
 @testset "Gravity Model Allocations" begin
@@ -18,8 +28,61 @@ end
     max_ord = 10
     P = zeros(max_deg, max_ord)
     dP = zeros(max_deg, max_ord)
-    @test length(check_allocs((model, x) -> GravityModels.gravitational_acceleration(model, x; max_degree=max_deg, max_order=max_ord, P=P, dP=dP), (IcgemFile{Float64,Val{:full},IcgemGfcCoefficient{Float64}}, Vector{Float64}))) == 0
-    @test length(check_allocs((model, x) -> GravityModels.gravitational_acceleration(model, x; max_degree=max_deg, max_order=max_ord, P=P, dP=dP), (IcgemFile{Float64,Val{:unnormalized},IcgemGfcCoefficient{Float64}}, Vector{Float64}))) == 0
-    @test length(check_allocs((x) -> GravityModels.gravitational_acceleration(model, x; max_degree=max_deg, max_order=max_ord, P=P, dP=dP), (Vector{Float64},))) == 0
-    @test length(check_allocs((r_itrf, x) -> GravityModels.gravitational_acceleration(model, r_itrf, x; max_degree=max_deg, max_order=max_ord, P=P, dP=dP), (Vector{Float64}, Float64))) == 0
+
+    @test length(
+        check_allocs(
+            (model, x) -> GravityModels.gravitational_acceleration(
+                model,
+                x;
+                max_degree = max_deg,
+                max_order = max_ord,
+                P = P,
+                dP = dP
+            ),
+            (IcgemFile{Float64, Val{:full}, IcgemGfcCoefficient{Float64}}, Vector{Float64})
+        )
+    ) == 0
+
+    @test length(
+        check_allocs(
+            (model, x) -> GravityModels.gravitational_acceleration(
+                model,
+                x;
+                max_degree = max_deg,
+                max_order = max_ord,
+                P = P,
+                dP = dP
+            ),
+            (IcgemFile{Float64, Val{:unnormalized}, IcgemGfcCoefficient{Float64}}, Vector{Float64})
+        )
+    ) == 0
+
+    @test length(
+        check_allocs(
+            (x) -> GravityModels.gravitational_acceleration(
+                model,
+                x;
+                max_degree = max_deg,
+                max_order = max_ord,
+                P = P,
+                dP = dP
+            ),
+            (Vector{Float64},)
+        )
+    ) == 0
+
+    @test length(
+        check_allocs(
+            (r_itrf, x) -> GravityModels.gravitational_acceleration(
+                model,
+                r_itrf,
+                x;
+                max_degree = max_deg,
+                max_order = max_ord,
+                P = P,
+                dP = dP
+            ),
+            (Vector{Float64}, Float64)
+        )
+    ) == 0
 end
